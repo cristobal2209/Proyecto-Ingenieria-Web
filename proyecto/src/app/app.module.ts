ed0bc4e95e6a1aca71ffe3254955d8d604e96940
cristobal2209 <cristobal.caceres.c@mail.pucv.cl>
2022-09-11 13:32:54 -0300

    configuración de proyecto con angular
    - Se crearon los componentes navbar, footer, foro, guia, home, login y signin.
    - Los componentes solamente se crearon, esto es para tener el esqueleto del proyecto.

---
+++ proyecto/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { GuiaComponent } from './guia/guia.component';
+import { ForoComponent } from './foro/foro.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    GuiaComponent,
+    ForoComponent,
+    LoginComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
